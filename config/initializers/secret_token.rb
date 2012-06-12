e5010e34906e817a58788c0b504e3b16bb247da2
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-06-12 19:56:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vizier::Application.config.secret_token = 'e68fb16bb74f145c951fff41eea0fcb346e80aff2e0975692d61ee3f3786f395bd00de572b292fd720c825010196cb79e650bd008b999a01539f37efb8f00ee7'
